60167814ae0abef78e64354e1fcfb9498c260421
meg wu <wumeg@sheridancollege.ca>
2024-10-03 08:52:11 -0400

    fixed link paths

--- appdev/HTMLassignment/articles/career1/career1.js
+++ appdev/HTMLassignment/articles/career1/career1.js
@@ -1,26 +1,37 @@
-let passage ="What is the best UX portfolio website builder in 2024? " +
-"Every UX designer knows the weight of a portfolio&#46" +
-"It&apos;s not just a digital footprint&#59 it&apos;s a showcase&#44 a personal narrative&#44 a gateway into their unique universe of design&#46" +
-"But&#44 just when the creative juices start flowing&#44 there&apos;s a hiccup—how to find the RIGHT tool for you in the sea of thousands of different tools&#63" +
-"The sheer number of platforms and tools can be overwhelming&#44 especially for junior designers&#46" +
-"This blog post aims to provide expert insights into the top portfolio builder tools in the market and help Designers make informed decisions&#46" +
-"Without further ado&#44 let&apos;s dive in&#46" +
- "Webflow" +
- "If you&apos;re looking to showcase advanced interactions and rich animations&#44 Webflow is what you need for your UX case studies&#46" +
-"Its responsive design capabilities ensure a smooth experience for your audience&#44 regardless of the device&#46" +
-"Pricing&#58 Mostly FREE" +
-"Rating&#58 8&#47 10" +
-"Why I Love It&#58" +
-"Webflow gives me the freedom to bring my most ambitious UX case studies to life without the need for extensive coding&#46" +
-"Its high customizability ensures that your UX portfolio stands out&#44 making it truly yours&#46" +
-"Pros&#58" +
-"Provides extensive customization options" +
-"Easily integrates animations and interactions" +
-"Features responsive design capabilities"
-
-
-let url = "https://uxplaybook.org/articles/best-ux-portfolio-website-builders-2024"
+let career1Art = {
+    passage: "What is the best UX portfolio website builder in 2024? " +
+         "Every UX designer knows the weight of a portfolio. " +
+         "It's not just a digital footprint, it's a showcase, a personal narrative, a gateway into their unique universe of design. " +
+         "But, just when the creative juices start flowing, there's a hiccup—how to find the RIGHT tool for you in the sea of thousands of different tools? " +
+         "The sheer number of platforms and tools can be overwhelming, especially for junior designers. " +
+         "This blog post aims to provide expert insights into the top portfolio builder tools in the market and help Designers make informed decisions. " +
+         "Without further ado, let's dive in. \n" +
+         "\n Webflow \n" +
+         "If you're looking to showcase advanced interactions and rich animations, Webflow is what you need for your UX case studies. " +
+         "Its responsive design capabilities ensure a smooth experience for your audience, regardless of the device. \n" +
+         "Pricing: Mostly FREE \n" +
+         "Rating: 8/10 \n" +
+         "Why I Love It \n" +
+        "Webflow gives me the freedom to bring my most ambitious UX case studies to life without the need for extensive coding. " +
+        "Its high customizability ensures that your UX portfolio stands out, making it truly yours. \n" +
+        "Pros: \n" +
+        "Provides extensive customization options. \n" +
+        "Easily integrates animations and interactions. \n" +
+        "Features responsive design capabilities. \n",
+    URL:"https://uxplaybook.org/articles/best-ux-portfolio-website-builders-2024",
+    author: "Christopher Nguyen",
+    image: "uxportfolio.webp",
+    title: "14 Best UX Portfolio Website Builders for Junior Designers in 2024"
+};
 
 let text = document.querySelector("#text");
+text.textContent = career1Art.passage;
 
-text.textContent = passage;
+let author = document.querySelector("#author");
+author.textContent = career1Art.author;
+
+let title = document.querySelector("#articleTitle");
+title.textContent = career1Art.title;
+
+let image = document.querySelector("#image");
+image = career1Art.image;
