72957f7ee47de34f170b28093463f2c7ffcb8a4c
meg wu <wumeg@sheridancollege.ca>
2025-02-07 15:17:24 -0500

    weh

--- interactivecoding/ic2/custom/sketch.js
+++ interactivecoding/ic2/custom/sketch.js
@@ -36,9 +36,14 @@ class HumidityConsole {
 
     this.offButton = createButton('Off');
     this.offButton.position(this.x + 290, this.y + 200);
+    let isOn = false;
     this.offButton.mousePressed(() => {
-      if (this.currentHumidity > this.maxHumidity) {
-        this.targetHumidity-=5;  
+      if (isOn == true) {
+
+        isOn == false;  
+      } else if (isOn == false){
+        
+        isOn == true;  
       }
     });
   }
@@ -52,17 +57,20 @@ class HumidityConsole {
     rect(this.x, this.y, 350, 250, 10);
 
     fill(100);
-    rect(this.x + 30, this.y + 50, 290, 110, 10);
+    rect(this.x + 20, this.y + 50, 310, 110, 10);
 
     fill(0);
     textSize(16);
     text("Humidity", this.x + 140, this.y + 35);
     textSize(14);
-    text("Current Humidity: ", this.x + 40, this.y + 75);
-    text(this.currentHumidity, this.x + 40, this.y + 150);
-    text("Target Humidity: ", this.x + 175, this.y + 75);
-    text(this.targetHumidity, this.x + 175, this.y + 150);
+    text("Current Humidity: ", this.x + 30, this.y + 75);
+    text("Target Humidity: ", this.x + 180, this.y + 75);
     text("Adjust Humidity:", this.x + 30, this.y + 190);
+
+    textFont('Courier New');
+    textSize (40);
+    text(this.currentHumidity, this.x + 110, this.y + 120);
+    text(this.targetHumidity, this.x + 260, this.y + 120);
   }
 }
 
